c843676cff62fff10aba08c8daa91b0af9ccbb42
Jerry Huang <jhuang9240@gmail.com>
2019-11-01 16:28:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,7 +14,11 @@ end
 
 def get_japanese_emoticon(path, emoji)
   lib = load_library(path)
-  lib[:get_emoticon][emoji]
+  return = lib[:get_emoticon][emoji] 
+  if return == nil do
+    return = "Sorry!, no emoji"
+  end
+  return
 end
 
 def get_english_meaning
